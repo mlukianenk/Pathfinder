ddf60317f6a44858b548f5b6da195ae6e9d9714c
Мария Лукьяненко <lukyanenko_m@MacBook-Pro-Maria.local>
2019-12-31 00:42:36 +0200

    invalid_line

--- inc/pathfinder.h
+++ inc/pathfinder.h
@@ -1,8 +1,9 @@
 #ifndef PATHFINDER_H
 #define PATHFINDER_H
-#include "../libmx/inc/libmx.h"
+#include "libmx/inc/libmx.h"
 
 void mx_printerr(char *filename);
+bool mx_isaplha(int c);
 
 bool mx_file_exist(char *filename);
 
--- src/mx_errors.c
+++ src/mx_errors.c
@@ -4,12 +4,17 @@ void mx_printerr(char *filename) {
     write(2, filename, mx_strlen(filename));
 }
 
+bool mx_isalpha(int c) {
+    if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) {
+        return true;
+    }
+    return false;
+}
+
 bool mx_file_exist(char *filename) {
     int fd = open(filename, O_RDONLY);
-    char buff;
-    int len = read(fd, &buff, 1);
 
-    if (len > 0)
+    if (fd > 0)
         return 1;
     mx_printerr("error: file ");
     mx_printerr(filename);
@@ -20,3 +25,32 @@ bool mx_file_exist(char *filename) {
     return 0;
 }
 
+bool mx_empty_file(char *filename) {
+    int fd = open(filename, O_RDONLY);
+    char buff;
+    int len = read(fd, &buff, 1);
+
+    if (len > 0)
+        return 1;
+    mx_printerr("error: file ");
+    mx_printerr(filename);
+    mx_printerr("is empty\n");
+
+    close(fd);
+
+    return 0;
+}
+
+bool mx_invalid_line(const char *file) {
+    char *lines = mx_file_to_str(file);
+
+    while(*lines !='\n') {
+        for (int i = 0; lines[i]; i++) {
+            if (!(mx_isalpha(lines[i]))) {
+                mx_printerr("error: line 1 is not valid ");
+                exit(0);
+            }
+        }
+    }
+    return 1;
+}
