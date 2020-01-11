0bc976c2e55c6ac1a146e6ce9efa6c2d5e6f26a3
Мария Лукьяненко <lukyanenko_m@MacBook-Pro-Maria.local>
2020-01-11 19:06:16 +0200

    pathfinder.h

--- inc/pathfinder.h
+++ inc/pathfinder.h
@@ -9,27 +9,36 @@ typedef struct s_form {
     char **islands_names; //all islands
 } t_form;
 
-void mx_all_functions(char *argv[], t_form *info);
-void mx_printerr(char *filename);
+void mx_all_functions(char *argv[], t_form *info); //main.c
+t_form *mx_init(int argc, char **argv); //mx_init.c
+
+//mx_basic_func.c
 bool mx_isalpha(int c);
 bool mx_isdigit(int c);
+int mx_atoi(const char *str);
+void mx_printerr(char *filename);
 void mx_printerr_line(int number);
+
+//mx_islands_info.c
+
+char *mx_write_zero(char *str);
+int mx_multiarr_len(char **all_islands);
+void mx_exclusive_islands(t_form *info);
+int mx_multilen_no_zero(char **all_islands);
+char **mx_find_exclusive_islands(char *all_lines);
+
+//mx_file_errors.c
+bool mx_file_exist(char *filename);
+bool mx_empty_file(char *filename);
+bool mx_invalid_amount(t_form *info);
+bool mx_file_info(t_form *info, const char *file);
+
+//mx_line_errors.c
 bool mx_invalid_digit(t_form *info, int i, int j);
 bool mx_invalid_lines(t_form *info);
 bool mx_invalid_distance(t_form *info);
-t_form *mx_init(int argc, char **argv);
-bool mx_file_exist(char *filename);
-bool mx_empty_file(char *filename);
 bool mx_invalid_first(t_form *info);
-char **mx_islands_info(t_form *info);
-bool mx_invalid_amount(t_form *info);
-bool mx_file_info(t_form *info, const char *file);
-int mx_atoi(const char *str);
-int mx_multiarr_len(char **all_islands);
-int mx_multilen_no_zero(char **all_islands);
-void mx_exclusive_islands(t_form *info);
-char *mx_write_zero(char *str);
-char **mx_find_exclusive_islands(char *all_lines);
 
+// char **mx_islands_info(t_form *info);
 
 #endif
--- src/mx_all_functions.c
+++ src/mx_all_functions.c
@@ -9,5 +9,4 @@ void mx_all_functions(char *argv[], t_form *info) {
 	mx_invalid_lines(info);
 	mx_exclusive_islands(info);
 	mx_invalid_amount(info);
-
 }
